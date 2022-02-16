0086bbaf7284df53da0b9dae60ffcffe3a385988
TimothÃee Boiteau <timotheeboiteau@gmail.com>
2022-02-16 17:45:27 +0100

    2. Yelp MVP refacto

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,13 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [ :destroy ]
+  before_action :set_restaurant, only: [ :new, :create ]
 
   def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
-    @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
@@ -24,6 +23,10 @@ class ReviewsController < ApplicationController
 
   def set_review
     @review = Review.find(params[:id])
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
   def review_params
